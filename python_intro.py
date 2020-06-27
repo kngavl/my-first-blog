87cfaa764d37e206866440ea3798a470cfdd6492
Arthur <green_slash@hotmail.com>
2020-06-27 12:21:19 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,18 @@
+#this is a comment
+print("Hello, Django girls")
+if -2>2:
+    print("Eat dirt")
+elif 2>1:
+    print("hooman")
+else:
+    print("y9o")
+
+def hi(name):
+    print("hello " + name)
+    print("hi")
+
+hi("frank")
+girls = ["shelly", "anja", "terry"]
+
+for name in girls:
+    print(name)
